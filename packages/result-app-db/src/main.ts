bd5dcaed1e8da65a191034dc5880e792a4c04c75
Ryan Tablada <ryan.tablada@gmail.com>
2022-07-08 19:34:43 -0500

    feat: monorepo dockerfile

--- packages/result-app-db/src/main.ts
+++ packages/result-app-db/src/main.ts
@@ -2,6 +2,6 @@ import AppCore from 'result-app-core';
 
 export default class AppCoreDb extends AppCore {
   getMessage(): string {
-    return `${this.message} bear`;
+    return `${this.message} walker`;
   }
 }
